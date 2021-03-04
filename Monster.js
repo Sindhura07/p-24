1e1eb12aeee0b06de7a755b3afc1df70727459a9
Sindhura07 <76103741+Sindhura07@users.noreply.github.com>
2021-03-04 19:04:00 +0530

    Add files via upload

---
+++ Monster.js
@@ -0,0 +1,25 @@
+class Monster{
+    constructor(x,y,width,height,angle){
+    var options={
+    'frictionAir':0.005,
+    'density':1
+    }
+    
+    this.body=Bodies.rectangle(x,y,width,height,angle,options);
+    this.width=width;
+    this.height=height;
+    this.image = loadImage("Monster-01.png");
+    World.add(world,this.body)
+}
+display(){
+  var pos =this.body.position
+  var angle=this.body.angle
+  push()
+  translate(pos.x,pos.y)
+ // rotate(angle)
+  fill("teal")
+  imageMode(CENTER);
+  image(this.image, 0, 0, this.width, this.height);
+  pop()
+}
+}
